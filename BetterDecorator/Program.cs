79a11cad4afc1a0ee01e12682b73443de423267b
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-13 11:33:08 +0000

    Parsing the arguments to use them on the Decor method, BetterDecorator project

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -6,7 +6,14 @@ namespace BetterDecorator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //parsing the arguments to use them in the Decor method
+            char dec = Char.Parse(args[1]);
+            int n = Char.Parse(args[2]);
+            Console.WriteLine(Decor(args[0], dec, n));
+        }
+
+        private static string Decor(string s, char dec, int n){
+               return $"{dec}{dec}{dec}{s}{dec}{dec}{dec}"; 
         }
     }
 }
