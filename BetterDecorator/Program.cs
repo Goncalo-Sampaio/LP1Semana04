448f52ef4f1cbae7d83bbde29d125fd894c3f60c
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-13 11:50:03 +0000

    Improved Decor method, BetterDecorator project

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -7,13 +7,20 @@ namespace BetterDecorator
         static void Main(string[] args)
         {
             //parsing the arguments to use them in the Decor method
-            char dec = Char.Parse(args[1]);
-            int n = Char.Parse(args[2]);
+            char dec = char.Parse(args[1]);
+            int n = int.Parse(args[2]);
             Console.WriteLine(Decor(args[0], dec, n));
         }
 
         private static string Decor(string s, char dec, int n){
-               return $"{dec}{dec}{dec}{s}{dec}{dec}{dec}"; 
+            string decor = "";
+
+            //looping to create a string with the char n number of times
+            for (int i = 0; i < n; i++) {
+                decor += dec;
+            }
+
+            return $"{decor} {s} {decor}"; 
         }
     }
 }
