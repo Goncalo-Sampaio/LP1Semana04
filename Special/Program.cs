41f40ac59fdc6476962e761ff86f4825eed8ed77
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-13 12:28:43 +0000

    Created GetSpecial method, Special project

--- Special/Program.cs
+++ Special/Program.cs
@@ -8,5 +8,24 @@ namespace Special
         {
             Console.WriteLine("Hello, World!");
         }
+
+        private static int GetSpecial(int i)
+        {
+            int special;
+            if (i == 0)
+            {
+                special = 0;
+            }
+            else if (i == 1)
+            {
+                special = 1;
+            }
+            else
+            {
+                special = GetSpecial(i - 1) + 2 * GetSpecial(i - 2);
+            }
+
+            return special;
+        }
     }
 }
