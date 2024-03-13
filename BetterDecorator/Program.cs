642b48fb00a0ea57c890f3c4ef03d19cd5c0b0bb
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-13 12:02:18 +0000

    Added XML documentation to BetterDecorator project

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -2,8 +2,16 @@
 
 namespace BetterDecorator
 {
+    /// <summary>
+    /// Class that will use the Main method to call Decor method using the
+    /// arguments inserted by the user when running the program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The Main method that will call the Decor method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //parsing the arguments to use them in the Decor method
@@ -12,6 +20,13 @@ namespace BetterDecorator
             Console.WriteLine(Decor(args[0], dec, n));
         }
 
+        /// <summary>
+        /// Method to add a char n number of times before and after a string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int n){
             string decor = "";
 
